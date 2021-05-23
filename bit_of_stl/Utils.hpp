4fb05a94282ca6bc6c27511000f1e4ade3589ace
Tenagrim <tenagrim@gmail.com>
2021-05-23 12:53:08 +0300

    Pair

--- bit_of_stl/Utils.hpp
+++ bit_of_stl/Utils.hpp
@@ -2,12 +2,60 @@
 #define UTILS_HPP
 
 namespace ft {
+
 	template<typename T>
 	void swap(T&a, T&b) {
 		T c = a;
 		a = b;
 		b = c;
 	}
+
+	template<
+			class T1,
+			class T2
+	> struct Pair{
+		typedef		T1	first_type;
+		typedef		T2	second_type;
+
+		first_type 		first;
+		second_type		second;
+
+		Pair(): first(), second(){}
+		Pair(const T1 &first, const T1 &second): first(first), second(second){}
+		Pair &operator=(const Pair &other) { first = other.first; second = other.second;}
+		void swap(Pair &other) {ft::swap(first, other.first); ft::swap(second, other.second);}
+	};
+
+	template< class T1, class T2 >
+	bool operator==( const ft::Pair<T1,T2>& lhs, const ft::Pair<T1,T2>& rhs ){
+		return lhs.first == rhs.second && lhs.first == rhs.second;
+	}
+
+	template< class T1, class T2 >
+	bool operator!=( const ft::Pair<T1,T2>& lhs, const ft::Pair<T1,T2>& rhs ){
+		return !(lhs == rhs);
+	}
+
+	template< class T1, class T2 >
+	bool operator<( const ft::Pair<T1,T2>& lhs, const ft::Pair<T1,T2>& rhs ){
+		return lhs.first < rhs.second || lhs.first < rhs.second;
+	}
+
+	template< class T1, class T2 >
+	bool operator<=( const ft::Pair<T1,T2>& lhs, const ft::Pair<T1,T2>& rhs ){
+		return !(lhs > rhs);
+	}
+
+	template< class T1, class T2 >
+	bool operator>( const ft::Pair<T1,T2>& lhs, const ft::Pair<T1,T2>& rhs ){
+		return lhs.first > rhs.second || lhs.first > rhs.second;
+	}
+
+	template< class T1, class T2 >
+	bool operator>=( const ft::Pair<T1,T2>& lhs, const ft::Pair<T1,T2>& rhs ){
+		return !(lhs < rhs);
+	}
+
 }
 
 #endif //UTILS_HPP
